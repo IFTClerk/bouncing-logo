4fc5df636fccadc3c514d1848dae71429150d2e5
Yiwen Yang <yangyiwen1999@outlook.com>
2022-09-21 10:02:04 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,122 @@
+const svg_logo = `
+      <svg class="logo" viewbox="0 0 155 130">
+      	<path d="m 63.74024,6.5439659 c -4.05717,0.850154 -8.0696,1.931608 -12.09054,2.942973 -0.76751,0.218256 -1.55385,0.37987 -2.29618,0.6781201 -0.38666,2.903435 -0.49911,5.840614 -0.74992,8.759425 -1.23588,16.85708 -2.20843,33.731376 -2.90438,50.619481 -0.79583,-18.088782 -1.50837,-36.183797 -2.46289,-54.263906 -0.077,-1.093355 -0.0853,-2.194633 -0.27344,-3.277109 -2.19476,0.381362 -4.2896,1.197029 -6.43742,1.775747 -2.99497,0.921447 -6.02945,1.731615 -8.98969,2.755732 -0.65986,0.303088 -0.30258,1.181802 -0.4615,1.753384 -1.30338,18.507869 -2.5575,37.020547 -3.3692,55.556932 -0.82981,-18.449082 -1.62236,-36.899856 -2.48047,-55.347654 -4.98063,1.39532 -9.9047,2.988965 -14.85937,4.474609 2.10237,32.584754 4.23202,65.168325 6.40625,97.74804 -0.002,0.65654 0.71853,0.76784 1.19504,0.51172 5.71346,-1.7674 11.44887,-3.47403 17.14675,-5.28516 0.7544,-0.36718 0.38626,-1.39824 0.56062,-2.07188 1.11546,-14.780455 2.26125,-29.558655 3.33587,-44.342175 1.05755,14.72023 2.24437,29.43762 3.92773,44.099605 6.269,-1.83176 12.56382,-3.57868 18.90235,-5.1543 2.20556,-34.000005 4.3643,-68.003178 6.52929,-102.0058501 l -0.6289,0.07227 z m 37.16797,12.9921871 c -0.48011,-0.0015 -0.95651,0.04903 -1.43164,0.113281 0.81444,12.387425 1.80799,24.762709 2.70852,37.144205 1.11639,14.998176 2.23516,29.996186 3.35007,44.994461 0.71009,0.53602 1.69006,0.26396 2.52442,0.38355 4.58605,0.12028 9.17326,0.61468 13.74091,1.19687 0.51906,-0.0233 0.1952,-1.06736 0.37277,-1.50398 0.88914,-9.996895 1.88707,-19.985545 2.58455,-29.998305 0.7642,10.82176 1.54996,21.64213 2.40626,32.457025 4.94626,1.03769 9.81301,2.36004 14.60351,3.99804 0.39295,0.0239 1.2477,0.59155 1.35157,0.1618 1.82839,-26.739715 3.62765,-53.481483 5.38085,-80.226244 -3.93111,-1.597049 -7.91667,-3.104246 -12.01562,-4.208984 -0.71657,-0.184817 -0.64228,0.716472 -0.71955,1.186365 -1.12744,11.583981 -2.03785,23.193166 -2.6828,34.813635 -0.76322,-12.478189 -1.48966,-24.958632 -2.27343,-37.435547 -1.60698,-0.578722 -3.33457,-0.750978 -5.00032,-1.113294 -2.52397,-0.472161 -5.06298,-0.877214 -7.61687,-1.148425 -0.95464,13.085824 -1.85988,26.175269 -2.75,39.265625 -0.90961,-13.248387 -1.78383,-26.499227 -2.6582,-39.75 -3.94298,-0.359245 -7.91741,-0.370495 -11.875,-0.330078 z m -17.73438,0.02539 c -4.27454,0.05173 -8.42166,2.129293 -11.10365,5.444676 -3.62855,4.322225 -5.2844,9.91747 -5.97939,15.43004 -1.08689,8.359057 -0.71584,16.808765 -0.80954,25.215126 0.0774,7.57071 -0.17565,15.15903 0.44431,22.71275 0.3793,4.29512 1.20013,8.71392 3.52749,12.415775 1.67168,2.59279 4.60231,4.18732 7.64327,4.47614 4.50349,0.41669 9.30969,-0.42786 12.98618,-3.19341 3.69762,-2.765465 5.78614,-7.107015 7.13737,-11.411645 1.74254,-5.92693 1.70037,-12.17237 1.76498,-18.29483 0.0283,-9.480509 0.11114,-18.962161 -0.0116,-28.442171 -0.0707,-5.052574 -0.54312,-10.21287 -2.58806,-14.892851 -1.4571,-3.720331 -4.16228,-7.145076 -7.96388,-8.633841 -1.59682,-0.661572 -3.3291,-0.87783 -5.04746,-0.825759 z m -0.84766,15.527343 c 0.9378,0.238896 0.86732,1.449503 1.01423,2.210978 0.26044,4.291762 0.1413,8.597752 0.19165,12.895658 5.9e-4,11.389622 0.10013,22.780363 -0.0485,34.169253 -0.1014,1.60367 0.063,3.35376 -0.73737,4.80374 -0.37625,0.61748 -1.44905,0.74501 -1.70708,-0.0464 -0.58509,-1.61706 -0.48308,-3.38809 -0.56937,-5.08276 -0.0951,-13.77997 -0.0699,-27.560895 -0.0159,-41.341031 0.0607,-2.175207 -0.0396,-4.335706 0.48568,-6.462933 0.1818,-0.612435 0.68739,-1.21621 1.38671,-1.146485 z" sodipodi:nodetypes="scccccccccccccccccccccscccccccccccccccccccccsccccccccccccssccccccccccc" />
+      </svg>
+`
+let logos = document.querySelectorAll('.logo');
+const logo_width = logos[0].clientWidth;
+const logo_height = logos[0].clientHeight;
+const fps = 60;
+let xmax = window.innerWidth - logo_width;
+let ymax = window.innerHeight - logo_height;
+
+// logo position
+let xpos = [Math.floor(Math.random() * xmax)];
+let ypos = [Math.floor(Math.random() * ymax)];
+// logo velocity
+let xvel = [5];
+let yvel = [5];
+
+function newLogo() {
+	document.getElementById("logos").innerHTML += svg_logo;
+	xpos.push(Math.floor(Math.random() * xmax));
+	ypos.push(Math.floor(Math.random() * ymax));
+	xvel.push(5);
+	yvel.push(5);
+
+	// update logo node list
+	logos = document.querySelectorAll('.logo');
+}
+
+function removeLogo() {
+	if (!logos.length) { return; }
+	logos[logos.length-1].remove();
+	// update logo node list
+	logos = document.querySelectorAll('.logo');
+	xpos.pop();
+	ypos.pop();
+	xvel.pop();
+	yvel.pop();
+}
+
+// function to update logo position
+function redrawLogos() {
+	for (const logo of logos.entries()) {
+		let logo_idx = logo[0];
+		let logo_node = logo[1];
+		logo_node.style.left = xpos[logo_idx] + 'px';
+		logo_node.style.top = ypos[logo_idx] + 'px';
+
+		if (window.getComputedStyle(logo_node).fill === "none") {
+			logo_node.style.fill = randomColour();
+		}
+	}
+}
+
+// function to update logo positions and velocities
+function updateLogos() {
+	for (const logo of logos.entries()) {
+		let logo_idx = logo[0];
+		let logo_node = logo[1];
+
+		// hit left/right edge
+		if (xpos[logo_idx] >= xmax || xpos[logo_idx] < 0){
+			xvel[logo_idx] = -xvel[logo_idx];
+			logo_node.style.fill = randomColour();
+		}
+		// hit top/bottom edge
+		if (ypos[logo_idx] >= ymax || ypos[logo_idx] < 0){
+			yvel[logo_idx] = -yvel[logo_idx];
+			logo_node.style.fill = randomColour();
+		}
+
+		xpos[logo_idx] += xvel[logo_idx];
+		ypos[logo_idx] += yvel[logo_idx];
+	}
+}
+
+function boundLogos() {
+	for (const logo of logos.entries()) {
+		let logo_idx = logo[0];
+		let logo_node = logo[1];
+		xpos[logo_idx] = Math.min(xmax, xpos[logo_idx]);
+		ypos[logo_idx] = Math.min(ymax, ypos[logo_idx]);
+	}
+}
+
+// set random colour to logo
+function randomColour() {
+	let colour = "#" + [...Array(6)]
+		.map(() => Math.floor(Math.random() * 16).toString(16))
+		.join('')
+		.toUpperCase();
+	return colour;
+}
+
+setInterval(()=>{
+	if (!logos.length) { return; }
+	updateLogos();
+
+	redrawLogos();
+},1000/fps)
+
+window.addEventListener('resize', () => {
+	xmax = window.innerWidth - logo_width;
+	ymax = window.innerHeight - logo_height;
+	boundLogos()
+});
+
+document.body.addEventListener('mousedown', (ev) => {
+	switch (ev.button) {
+	case 0:
+		//left click
+		newLogo();
+		break;
+	case 1:
+		//middle click
+		removeLogo();
+		break;
+	default:
+		// do nothing
+	}
+});
